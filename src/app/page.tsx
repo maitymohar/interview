7deb66da939cd8707def425a80b21728cb17d1b7
maitymohar <maitymohar2005@gmail.com>
2025-03-19 14:44:07 +0530

    Clerk Authentication Enabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        SignIn
-      </Button>
+    <div className="mt-10">
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
+     
     </div>
   );
 }
